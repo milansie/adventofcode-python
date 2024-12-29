e9ae85571f598f2fa4089e173a7eee7490f76dcb
milansie <milan.siebenburger@gmail.com>
2024-12-29 09:52:51 +0100

    improvements

--- common/advent_day.py
+++ common/advent_day.py
@@ -1,7 +1,7 @@
 import inspect
 import os
+import time
 from abc import abstractmethod
-from calendar import firstweekday
 from pathlib import Path
 
 
@@ -44,37 +44,43 @@ class AdventDay:
         input_test_1 = self._load_input("1_input.txt")
         result_test_1 = "\n".join(self._load_input("1_result.txt"))
 
-
         # first with testing data
         self.IN_TEST = True
+        start_time = time.perf_counter()
         first_test_result = self.first(input_test_1)
 
+        consumed_time = int((time.perf_counter() - start_time) * 1000)
         if first_test_result != result_test_1:
-            self._log(f"ERROR First test failed: expected '{result_test_1}', but was '{first_test_result}'")
+            self._log(f"ERROR First test failed: expected '{result_test_1}', but was '{first_test_result}' in {consumed_time} ms")
         else:
-            self._log(f"First test passed: expected '{result_test_1}', was '{first_test_result}'")
+            self._log(f"First test passed: expected '{result_test_1}', was '{first_test_result}' in {consumed_time} ms")
 
         # first with production data
         self.IN_TEST = False
         input_data = self._load_input("input.txt")
+        start_time = time.perf_counter()
         first_result = self.first(input_data)
-        self._log(f"First result: {first_result}")
+        consumed_time = int((time.perf_counter() - start_time) * 1000)
+        self._log(f"First result: {first_result} in {consumed_time} ms")
 
         # second with testing data
         self.IN_TEST = True
         input_test_2 = self._load_input("2_input.txt")
         result_test_2 = "\n".join(self._load_input("2_result.txt"))
 
+        start_time = time.perf_counter()
         second_test_result = self.second(input_test_2)
+        consumed_time = int((time.perf_counter() - start_time) * 1000)
         if second_test_result != result_test_2:
-            self._log(f"ERROR Second test failed: expected '{result_test_2}', but was '{second_test_result}'")
+            self._log(f"ERROR Second test failed: expected '{result_test_2}', but was '{second_test_result}' in {consumed_time} ms")
         else:
-            self._log(f"Second test passed: expected '{result_test_2}', was '{second_test_result}'")
+            self._log(f"Second test passed: expected '{result_test_2}', was '{second_test_result}' in {consumed_time} ms")
 
         # second with production data
         self.IN_TEST = False
+        start_time = time.perf_counter()
         second_result = self.second(input_data)
-        self._log(f"Second result: {second_result}")
+        self._log(f"Second result: {second_result} in {int((time.perf_counter() - start_time) * 1000)} ms")
 
         print("---------------------------------------")
         print('\n'.join(self.log_lines))
